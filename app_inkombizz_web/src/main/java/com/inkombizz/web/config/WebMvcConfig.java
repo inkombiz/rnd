f055a5ecc078a02c836d9034d3f15b05d235d296
imamsolikhin <imamsolikhin@github.com>
2020-12-15 20:28:03 +0700

    up

--- app_inkombizz_web/src/main/java/com/inkombizz/web/config/WebMvcConfig.java
+++ app_inkombizz_web/src/main/java/com/inkombizz/web/config/WebMvcConfig.java
@@ -1,15 +1,15 @@
-//package com.inkombizz.web.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//@Configuration
-//public class WebMvcConfig extends WebMvcConfigurerAdapter {
-//
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.inkombizz.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
