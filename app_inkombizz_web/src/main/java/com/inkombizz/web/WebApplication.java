4f86944e49d80965092ee8f0b6e3786ec167f1f8
imamsolikhin <imamsolikhin@github.com>
2020-12-17 23:22:52 +0700

    up

--- app_inkombizz_web/src/main/java/com/inkombizz/web/WebApplication.java
+++ app_inkombizz_web/src/main/java/com/inkombizz/web/WebApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer{
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+       return application.sources(WebApplication.class);
+  }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
