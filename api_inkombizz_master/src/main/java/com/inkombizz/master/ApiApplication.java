a3642a47e2c12720ff6f0319f4e31016d7086d8b
inkombiz <inkombiz.developer@gmail.com>
2020-12-17 18:56:03 +0700

    rubah ApiApp

--- api_inkombizz_master/src/main/java/com/inkombizz/master/ApiApplication.java
+++ api_inkombizz_master/src/main/java/com/inkombizz/master/ApiApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class })
-public class ApiApplication {
+public class ApiApplication 
+		extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
