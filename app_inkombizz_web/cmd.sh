e5c25b4170287d33ea3ef52b09b3027142a84d76
imamsolikhin <imamsolikhin@github.com>
2020-12-17 23:59:18 +0700

    Create cmd.sh

---
+++ app_inkombizz_web/cmd.sh
@@ -0,0 +1,4 @@
+app="web_app"
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=${app} --format="{{.ID}}"))
+docker build -t ${app} .
+docker run -i -d -p 8282:8080 --name=${app} -v $PWD:/app ${app}
