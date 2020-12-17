800f37b6f6cc44a53f80a32e64fdd591d499f28a
imamsolikhin <imamsolikhin@github.com>
2020-12-18 00:02:57 +0700

    Update cmd.sh

--- app_inkombizz_web/cmd.sh
+++ app_inkombizz_web/cmd.sh
@@ -1,4 +1,5 @@
 app="web_app"
+mvn package
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=${app} --format="{{.ID}}"))
 docker build -t ${app} .
 docker run -i -d -p 8282:8080 --name=${app} -v $PWD:/app ${app}
